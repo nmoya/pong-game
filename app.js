a4136e17eb82816e5755a9e4cad01c0e80052915
Nikolas Moya <moya@work.co>
2017-05-09 01:36:51 +0200

    Add port 80 support for heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('./web-server/app');
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Pong web-server!`);
   console.log(`Mode: ${app.get('env')}`);
   console.log(`Port: listening on port 3000`);
